4ba71767ca1f2f87dcf8fed47068b53d9aa21b57
Markus Blanka <blankamarkus27@outlook.com>
2023-05-17 23:03:02 +0300

    Add new functionality

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
-app.set('view engine' , 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/adoptionDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/adoptionDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
-    email : String,
+    email: String,
     password: String
 });
 
 const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -30,7 +30,7 @@ const dogSchema = new mongoose.Schema({
     behaviour: String,
     description: String,
     reason: String,
-    
+
 });
 
 const Dog = new mongoose.model("Dog", dogSchema);
@@ -44,67 +44,71 @@ const catSchema = new mongoose.Schema({
     behaviour: String,
     description: String,
     reason: String,
-    
+
 });
 
 const Cat = new mongoose.model("Cat", catSchema);
 
-app.get("/", function (req,res){
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login", function (req,res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/register", function (req,res){
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.get("/firstpage", function (req,res){
-    res.render("home");
+app.get("/firstpage", function (req, res) {
+    res.render("firstpage");
 });
 
-app.post("/register", function(req,res){
-    
+app.post("/register", function (req, res) {
+
     const newUser = new User({
         email: req.body.username,
         password: req.body.password
     });
     newUser.save()
-    .then(() => {
-        res.render("firstpage");
-    })
-    .catch(err => {
-        console.log(err);
-    });
+        .then(() => {
+            res.render("firstpage");
+        })
+        .catch(err => {
+            console.log(err);
+        });
 
 
 });
 
-app.post("/makeapost", function(req,res){
+app.get("/makeapost", function (req, res) {
+    res.render("makeapost");
+});
 
-    if(req.body.typeOfAnimal === "Dog") {
-    const newDog = new Dog({
-        name: req.body.animalName,
-        sex : req.body.sex,
-        breed: req.body.breed,
-        age: req.body.age,
-        behaviour: req.body.behaviour,
-        description: req.body.description,
-        reason: req.body.reason
-    });
-    newDog.save()
-    .then(() => {
-        res.render("dogs");
-    })
-    .catch(err => {
-        console.log(err);
-    });
+app.post("/makeapost", function (req, res) {
+
+    if (req.body.typeOfAnimal === "Dog") {
+        const newDog = new Dog({
+            name: req.body.animalName,
+            sex: req.body.sex,
+            breed: req.body.breed,
+            age: req.body.age,
+            behaviour: req.body.behaviour,
+            description: req.body.description,
+            reason: req.body.reason
+        });
+        newDog.save()
+            .then(() => {
+                res.redirect("/dogs"); 
+            })
+            .catch(err => {
+                console.log(err);
+            });
     } else {
         const newCat = new Cat({
             name: req.body.animalName,
-            sex : req.body.sex,
+            sex: req.body.sex,
             breed: req.body.breed,
             age: req.body.age,
             behaviour: req.body.behaviour,
@@ -112,51 +116,138 @@ app.post("/makeapost", function(req,res){
             reason: req.body.reason
         });
         newCat.save()
-        .then(() => {
-            res.render("cats");
-        })
-        .catch(err => {
-            console.log(err);
-        });
+            .then(() => {
+                res.redirect("/cats"); 
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
 
 });
 
-app.get("/dogs",function(req,res){
-    res.render("dogs");
+app.get("/dogs", function (req, res) {
+    Dog.find()
+        .then(dogs => {
+            res.render("dogs", { dogs: dogs });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 });
 
-app.get("/cats",function(req,res){
-    res.render("cats");
+app.get("/dogs/:id", function (req, res) {
+    const dogId = req.params.id;
+
+    Dog.findById(dogId)
+        .then(dog => {
+            if (dog) {
+                res.render("dogDetail", { dog: dog });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
-app.get("/makeapost",function(req,res){
+
+app.get("/cats", function (req, res) {
+    Cat.find()
+        .then(cats => {
+            res.render("cats", { cats: cats });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+app.get("/cats/:id", function (req, res) {
+    const catId = req.params.id;
+
+    Cat.findById(catId)
+        .then(cat => {
+            if (cat) {
+                res.render("catDetail", { cat: cat });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.post('/dogs/:id', async (req, res) => {
+    const dogId = req.params.id;
+  
+    try {
+      // Search by id
+      const dog = await Dog.findById(dogId);
+  
+      if (!dog) {
+        return res.status(404).send('Dog not found.');
+      }
+  
+      // Delete the dog from the database
+      await Dog.findByIdAndRemove(dogId);
+
+      res.redirect("/success");
+    } catch (error) {
+      res.status(500).send('An error occurred while processing the adoption request.');
+    }
+  });
+
+app.post('/cats/:id', async (req, res) => {
+    const catId = req.params.id;
+  
+    try {
+      // Search by id
+      const cat = await Cat.findById(catId);
+  
+      if (!cat) {
+        // Daca nu se
+        return res.status(404).send('Cat not found.');
+      }
+  
+      // Delete the cat from the database
+      await Cat.findByIdAndRemove(catId);
+
+      res.redirect("/success");
+    } catch (error) {
+      res.status(500).send('An error occurred while processing the adoption request.');
+    }
+  });
+  
+
+
+app.get("/makeapost", function (req, res) {
     res.render("makeapost");
 });
 
+app.get("/success", function (req, res) {
+    res.render("success");
+});
 
-app.post("/login",function(req,res){
+app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({email: username})
-    .then((foundUser) => {
-        if(foundUser){
-            if(foundUser.password === password){
-                res.render("firstpage");
+    User.findOne({ email: username })
+        .then((foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("firstpage");
+                }
             }
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 
 });
 
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000!");
 });
 
